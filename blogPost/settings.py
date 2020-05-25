1e547ac2cd5a4e460a266a849017433fe378da9f
Detravious Brinkley <d.brinkley97@gmail.com>
2020-05-25 12:35:34 -0400

    DEBUG = True to find error

--- blogPost/settings.py
+++ blogPost/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q+_0=-i&(d(sm*=mq*k^j=)1dbh@zd^!+@8es6*7o@78mx@lit'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["www.sheltered-island-56300.herokuapp.com", "127.0.0.1", "localhost"]
 
